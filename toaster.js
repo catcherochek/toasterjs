38dd59753062fb2fb4b39bcdcc3ff952ce76b2f4
klim2020 <klimprograms@gmail.com>
2021-01-25 10:28:18 +0300

    2nd commit

--- toaster.js
+++ toaster.js
@@ -7,7 +7,7 @@ class Toaster{
   afterDiv = "body";
   inTheEnd = false;
   attachEvents(tis){
-    document.querySelectorAll('.toast__close').forEach(elem => {
+    document.querySelectorAll('.toastt__close').forEach(elem => {
         elem.addEventListener('click',function(e){
           e.preventDefault();
           var parent = e.currentTarget.parentNode;
@@ -35,21 +35,21 @@ class Toaster{
 
     mountHTML(afterDiv="body",inTheEnd = true) {
         var newNode = document.createElement("div");
-        newNode.setAttribute("class", "toast__container"); 
+        newNode.setAttribute("class", "toastt__container"); 
         newNode.innerHTML = `
-        <div class="toast__cell">
-        <div class="toast toast--green">
-          <div class="toast__icon">
-            <svg version="1.1" class="toast__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <div class="toastt__cell">
+        <div class="toastt toastt--green">
+          <div class="toastt__icon">
+            <svg version="1.1" class="toastt__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
         <g><g><path d="M504.502,75.496c-9.997-9.998-26.205-9.998-36.204,0L161.594,382.203L43.702,264.311c-9.997-9.998-26.205-9.997-36.204,0    c-9.998,9.997-9.998,26.205,0,36.203l135.994,135.992c9.994,9.997,26.214,9.99,36.204,0L504.502,111.7    C514.5,101.703,514.499,85.494,504.502,75.496z"></path>
             </g></g>
             </svg>
           </div>
-          <div class="toast__content">
-            <p class="toast__type">Success</p>
-            <p class="toast__message">Anyone with access can view your invited visitors.</p>
+          <div class="toastt__content">
+            <p class="toastt__type">Success</p>
+            <p class="toastt__message">Anyone with access can view your invited visitors.</p>
           </div>
-          <div class="toast__close">
+          <div class="toastt__close">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.642 15.642" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 15.642 15.642">
           <path fill-rule="evenodd" d="M8.882,7.821l6.541-6.541c0.293-0.293,0.293-0.768,0-1.061  c-0.293-0.293-0.768-0.293-1.061,0L7.821,6.76L1.28,0.22c-0.293-0.293-0.768-0.293-1.061,0c-0.293,0.293-0.293,0.768,0,1.061  l6.541,6.541L0.22,14.362c-0.293,0.293-0.293,0.768,0,1.061c0.147,0.146,0.338,0.22,0.53,0.22s0.384-0.073,0.53-0.22l6.541-6.541  l6.541,6.541c0.147,0.146,0.338,0.22,0.53,0.22c0.192,0,0.384-0.073,0.53-0.22c0.293-0.293,0.293-0.768,0-1.061L8.882,7.821z"></path>
         </svg>
@@ -57,9 +57,9 @@ class Toaster{
         </div>
         
         
-        <div class="toast toast--blue">
-          <div class="toast__icon">
-        <svg version="1.1" class="toast__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
+        <div class="toastt toastt--blue">
+          <div class="toastt__icon">
+        <svg version="1.1" class="toastt__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
         <g>
             <g id="info">
                 <g>
@@ -71,40 +71,40 @@ class Toaster{
         
             </svg>
           </div>
-          <div class="toast__content">
-            <p class="toast__type">Info</p>
-            <p class="toast__message">Anyone with access can view your invited visitors.</p>
+          <div class="toastt__content">
+            <p class="toastt__type">Info</p>
+            <p class="toastt__message">Anyone with access can view your invited visitors.</p>
           </div>
-          <div class="toast__close">
+          <div class="toastt__close">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.642 15.642" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 15.642 15.642">
           <path fill-rule="evenodd" d="M8.882,7.821l6.541-6.541c0.293-0.293,0.293-0.768,0-1.061  c-0.293-0.293-0.768-0.293-1.061,0L7.821,6.76L1.28,0.22c-0.293-0.293-0.768-0.293-1.061,0c-0.293,0.293-0.293,0.768,0,1.061  l6.541,6.541L0.22,14.362c-0.293,0.293-0.293,0.768,0,1.061c0.147,0.146,0.338,0.22,0.53,0.22s0.384-0.073,0.53-0.22l6.541-6.541  l6.541,6.541c0.147,0.146,0.338,0.22,0.53,0.22c0.192,0,0.384-0.073,0.53-0.22c0.293-0.293,0.293-0.768,0-1.061L8.882,7.821z"></path>
         </svg>
           </div>
         </div>
         
-        <div class="toast toast--yellow">
-          <div class="toast__icon">
-        <svg version="1.1" class="toast__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 301.691 301.691" style="enable-background:new 0 0 301.691 301.691;" xml:space="preserve">
+        <div class="toastt toastt--yellow">
+          <div class="toastt__icon">
+        <svg version="1.1" class="toastt__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 301.691 301.691" style="enable-background:new 0 0 301.691 301.691;" xml:space="preserve">
         <g>
             <polygon points="119.151,0 129.6,218.406 172.06,218.406 182.54,0  "></polygon>
             <rect x="130.563" y="261.168" width="40.525" height="40.523"></rect>
         </g>
             </svg>
           </div>
-          <div class="toast__content">
-            <p class="toast__type">Warning</p>
-            <p class="toast__message">Anyone with access can view your invited visitors.</p>
+          <div class="toastt__content">
+            <p class="toastt__type">Warning</p>
+            <p class="toastt__message">Anyone with access can view your invited visitors.</p>
           </div>
-          <div class="toast__close">
+          <div class="toastt__close">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.642 15.642" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 15.642 15.642">
           <path fill-rule="evenodd" d="M8.882,7.821l6.541-6.541c0.293-0.293,0.293-0.768,0-1.061  c-0.293-0.293-0.768-0.293-1.061,0L7.821,6.76L1.28,0.22c-0.293-0.293-0.768-0.293-1.061,0c-0.293,0.293-0.293,0.768,0,1.061  l6.541,6.541L0.22,14.362c-0.293,0.293-0.293,0.768,0,1.061c0.147,0.146,0.338,0.22,0.53,0.22s0.384-0.073,0.53-0.22l6.541-6.541  l6.541,6.541c0.147,0.146,0.338,0.22,0.53,0.22c0.192,0,0.384-0.073,0.53-0.22c0.293-0.293,0.293-0.768,0-1.061L8.882,7.821z"></path>
         </svg>
           </div>
         </div>
 
-        <div class="toast toast--red">
-          <div class="toast__icon">
-        <svg version="1.1" class="toast__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
+        <div class="toastt toastt--red">
+          <div class="toastt__icon">
+        <svg version="1.1" class="toastt__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
         <g>
             <g id="info">
                 <g>
@@ -116,11 +116,11 @@ class Toaster{
         
             </svg>
           </div>
-          <div class="toast__content">
-            <p class="toast__type">Info</p>
-            <p class="toast__message">Anyone with access can view your invited visitors.</p>
+          <div class="toastt__content">
+            <p class="toastt__type">Info</p>
+            <p class="toastt__message">Anyone with access can view your invited visitors.</p>
           </div>
-          <div class="toast__close">
+          <div class="toastt__close">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.642 15.642" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 15.642 15.642">
           <path fill-rule="evenodd" d="M8.882,7.821l6.541-6.541c0.293-0.293,0.293-0.768,0-1.061  c-0.293-0.293-0.768-0.293-1.061,0L7.821,6.76L1.28,0.22c-0.293-0.293-0.768-0.293-1.061,0c-0.293,0.293-0.293,0.768,0,1.061  l6.541,6.541L0.22,14.362c-0.293,0.293-0.293,0.768,0,1.061c0.147,0.146,0.338,0.22,0.53,0.22s0.384-0.073,0.53-0.22l6.541-6.541  l6.541,6.541c0.147,0.146,0.338,0.22,0.53,0.22c0.192,0,0.384-0.073,0.53-0.22c0.293-0.293,0.293-0.768,0-1.061L8.882,7.821z"></path>
         </svg>
@@ -133,7 +133,7 @@ class Toaster{
 
 
 
-.toast__container {
+.toastt__container {
   font-family: 'Raleway', sans-serif;
   background-color:#efefef;
   display: table;
@@ -143,7 +143,7 @@ class Toaster{
   position:absolute;
 }
 
-.toast__cell{
+.toastt__cell{
 display:inline-block;
 }
 
@@ -151,11 +151,11 @@ display:inline-block;
 margin-top:20px;
 }
 
-.toast__svg{
+.toastt__svg{
 fill:#fff;
 }
 
-.toast {
+.toastt {
 
   display: none;
   text-align:left;
@@ -170,7 +170,7 @@ fill:#fff;
 }
 
 
-.toast:before {
+.toastt:before {
   content: '';
   position: absolute;
   top: 0;
@@ -182,7 +182,7 @@ border-bottom-left-radius: 4px;
 
 }
 
-.toast__icon{
+.toastt__icon{
 position:absolute;
 top:50%;
 left:22px;
@@ -194,26 +194,26 @@ border-radius:50%;
 display:inline-block;
 }
 
-.toast__type {
+.toastt__type {
 color: #3e3e3e;
 font-weight: 700;
 margin-top: 0;
 margin-bottom: 8px;
 }
 
-.toast__message {
+.toastt__message {
   font-size: 14px;
   margin-top: 0;
 margin-bottom: 0;
   color: #878787;
 }
 
-.toast__content{
+.toastt__content{
 padding-left:70px;
 padding-right:60px;
 }
 
-.toast__close {
+.toastt__close {
   position: absolute;
   right: 22px;
   top: 50%;
@@ -224,36 +224,36 @@ fill:#878787;
   transform: translateY(-50%);
 }
 
-.toast--red .toast__icon{
+.toastt--red .toastt__icon{
 background-color:#de2b2b;
 }
 
-.toast--red:before{
+.toastt--red:before{
 background-color:#de2b2b;
 }
 
 
-.toast--green .toast__icon{
+.toastt--green .toastt__icon{
 background-color:#2BDE3F;
 }
 
-.toast--green:before{
+.toastt--green:before{
 background-color:#2BDE3F;
 }
 
-.toast--blue .toast__icon{
+.toastt--blue .toastt__icon{
 background-color:#1D72F3;
 }
 
-.toast--blue:before{
+.toastt--blue:before{
 background-color:#1D72F3;
 }
 
-.toast--yellow .toast__icon{
+.toastt--yellow .toastt__icon{
 background-color:#FFC007;
 }
 
-.toast--yellow:before{
+.toastt--yellow:before{
 background-color:#FFC007;
 }
   </style>
@@ -280,23 +280,23 @@ background-color:#FFC007;
           var block = "";
           switch(type){
             case this.MSG_ERROR:
-              block = ".toast--red";
+              block = ".toastt--red";
               break;
             case this.MSG_WARNING:
-              block = ".toast--blue";
+              block = ".toastt--blue";
               break;
             case this.MSG_SUCCESS:
-              block = ".toast--green";
+              block = ".toastt--green";
               break;
             case this.MSG_INFO:
-              block = ".toast--yellow";
+              block = ".toastt--yellow";
               break;
           }
-          document.querySelector(block+' .toast__message').innerHTML = text;
+          document.querySelector(block+' .toastt__message').innerHTML = text;
           this.fadeIn(document.querySelector(block)); 
         };
     hidemessage(){
-          document.querySelectorAll('.toast').forEach(elem =>{
+          document.querySelectorAll('.toastt').forEach(elem =>{
             if (elem.style.display == "block"){
               this.fadeOut(elem);
             }
